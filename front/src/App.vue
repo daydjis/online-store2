54122313b66df77375d19e1523d6e1f54d9b41d4
ilya mironyuk <ilya.mironyuk21@gmail.com>
2023-01-20 01:21:52 +0400

    Сделал авторизацию так же сделал тёнмую тему

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,42 +1,89 @@
 <template>
-  <div id="app">
-    <nav class="header">
-      <router-link to="/">Каталог</router-link> |
-      <router-link to="/about">Корзина</router-link> |
-      <div class="auth-container">
-        <router-link to="/auth/login">вход</router-link> |
-        <router-link to="/auth/register">регистрация</router-link>
-      </div>
-    </nav>
-    <router-view />
-  </div>
+    <div id="app">
+        <nav class="header">
+            <router-link to="/">Каталог</router-link> | <router-link to="/about">Корзина</router-link> |
+            <div class="auth-container" v-if="!this.$store.getters.AUTH">
+                <router-link to="/auth/login">вход</router-link> |
+                <router-link to="/auth/register">регистрация</router-link>
+            </div>
+            <div v-else class="auth-container">
+                <a href="" @click="deleteCookie()">Выйти</a> |
+                <!-- <a href="" class="">{{ this.$store.getters.USER_NICKNAME }}</a> -->
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    fill="currentColor"
+                    class="bi bi-person-circle"
+                    viewBox="0 0 16 16"
+                >
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path
+                        fill-rule="evenodd"
+                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                    />
+                </svg>
+            </div>
+            <div>
+                <div class="form-check form-switch">
+                    <input @click="checkTheme()" class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                    <label class="form-check-label" for="flexSwitchCheckDefault">Ночной режим</label>
+                </div>
+            </div>
+        </nav>
+        <router-view />
+    </div>
 </template>
 
-<script></script>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+export default class HomeView extends Vue {
+    checkTheme() {
+        if (!this.$store.getters.DARK_THEME) {
+            this.$store.commit('IS_DARK_THEME', true);
+            document.body.style.backgroundColor = 'black';
+        } else {
+            this.$store.commit('IS_DARK_THEME', false);
+            document.body.style.backgroundColor = 'white';
+        }
+    }
+    deleteCookie() {
+        const cookies = document.cookie.split(';');
+        localStorage.clear();
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i];
+            const eqPos = cookie.indexOf('=');
+            const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;';
+            document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        }
+    }
+}
+</script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 .header {
-  display: flex;
-  justify-content: space-evenly;
+    display: flex;
+    justify-content: space-evenly;
 }
 
 nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
--- front/src/components/authL/Login.vue
+++ front/src/components/authL/Login.vue
@@ -1,36 +1,40 @@
 <template>
-  <div class="login-from">
-    <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Логин</label>
-        <input v-model="User.login" class="form-control" />
-        <small id="emailHelp" class="form-text text-muted"
-          >We'll never share your email with anyone else.</small
-        >
-      </div>
+    <div>
+        <div class="login-from" v-if="!this.$store.getters.IS_AUTH">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Логин</label>
+                    <input v-model="User.login" class="form-control" />
+                    <small id="emailHelp" class="form-text text-muted"
+                        >We'll never share your email with anyone else.</small
+                    >
+                </div>
 
-      <div class="form-group">
-        <label for="exampleInputPassword1">Пароль</label>
-        <input v-model="User.password" type="password" class="form-control" />
-        <small id="passwordHelpBlock" class="form-text text-muted">
-          Your password must be 8-20 characters long, contain letters and
-          numbers.
-        </small>
-      </div>
-    </form>
-    <button v-on:click="post(User)" class="btn btn-primary">Отправить</button>
-  </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Пароль</label>
+                    <input v-model="User.password" type="password" class="form-control" />
+                    <small id="passwordHelpBlock" class="form-text text-muted">
+                        Your password must be 8-20 characters long, contain letters and numbers.
+                    </small>
+                </div>
+            </form>
+            <button v-on:click="post(User)" class="btn btn-primary">Отправить</button>
+        </div>
+        <div class="spinner-border" role="status" v-else>
+            <span class="sr-only"></span>
+        </div>
+    </div>
 </template>
 
 <script lang="ts" src="./auth.ts"></script>
 
 <style>
 .login-from {
-  margin: 0 auto;
-  max-width: 400px;
+    margin: 0 auto;
+    max-width: 400px;
 }
 
 .btn {
-  margin-top: 50px;
+    margin-top: 50px;
 }
 </style>
